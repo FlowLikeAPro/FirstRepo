6de43abc7c4249079ddd14243b81f034443bf18a
Jannic Jost <jannic.jost@peakpeak.de>
2023-09-23 21:16:28 +0200

    MyCalc Update

--- myOwnCalc.py
+++ myOwnCalc.py
@@ -1,11 +1,30 @@
-firNum = input("First Number: ")
-secNum = input("Second Number: ")
+def add(firNum, secNum):
+  return firNum + secNum
+def multiply(firNum, secNum):
+  return firNum * secNum
+def division(firNum, secNum):
+  if secNum != 0:
+        return firNum / secNum
+  else:
+        return "Error: Division by zero my friend"
+def sub(firNum, secNum):
+  return firNum - secNum
 
-operator = input("Select operator: ")
+firNum = float(input("First Number: "))
+secNum = float(input("Second Number: "))
 
-
-
-result = int(firNum) + int(secNum)   
-
-
-print("Result is: " + str(result))
+def operator():
+  operator = input("Select operator: ")
+  if operator == '+':
+    print(add (firNum, secNum))
+  elif operator == '*':
+    print(multiply (firNum, secNum))
+  elif operator == '-':
+    print(sub (firNum, secNum))
+  elif operator == '/':
+    print(division (firNum, secNum))
+  else:
+    print("WTF")
+    
+if __name__ == "__main__":
+    operator()
